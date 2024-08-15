40b29a7722040800e70df60a6561c06c7a42082e
avi.o <aviosipov@gmail.com>
2024-08-15 13:23:54 +0300

    refactor: Update project title and item days in price quote data

--- src/pages/PriceQuote.tsx
+++ src/pages/PriceQuote.tsx
@@ -1,27 +1,43 @@
-import React from 'react';
-import { ContentPageContainer } from '../components/ContentPageContainer';
-import priceQuoteData from '../data/priceQuoteData.json';
+import React from "react";
+import { ContentPageContainer } from "../components/ContentPageContainer";
+import priceQuoteData from "../data/priceQuoteData.json";
 
 const PriceQuote: React.FC = () => {
-  const totalPrice = priceQuoteData.items.reduce((sum, item) => sum + item.price, 0);
-  const totalDays = priceQuoteData.items.reduce((sum, item) => sum + item.days, 0);
+  const totalPrice = priceQuoteData.items.reduce(
+    (sum, item) => sum + item.price,
+    0
+  );
+  const totalDays = priceQuoteData.items.reduce(
+    (sum, item) => sum + item.days,
+    0
+  );
 
   return (
     <ContentPageContainer>
-      <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-md" >
-        <h1 className="text-3xl font-bold mb-4 pb-2 border-b-2 border-blue-500 text-gray-800">{priceQuoteData.projectTitle}</h1>
-        <p className="mb-4">
-          <strong>לכבוד:</strong> {priceQuoteData.customerName}<br />
-          <strong>מאת:</strong> {priceQuoteData.companyName}
-        </p>
-        
+      <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-3xl font-bold mb-4 pb-2 border-b-2 border-blue-500 text-gray-800">
+          {priceQuoteData.projectTitle}
+        </h1>
+        <div className="my-5 space-y-1">
+          <div className="flex">
+            <div className="w-16 font-semibold">לכבוד</div>
+            <span>{priceQuoteData.customerName}</span>
+          </div>
+          <div className="flex">
+            <div className="w-16 font-semibold">מאת</div>
+            <span>{priceQuoteData.companyName}</span>
+          </div>
+        </div>
+
         {priceQuoteData.items.map((item, index) => (
           <QuoteItem key={index} {...item} />
         ))}
 
         <div className="mt-8 pt-4 border-t-2 border-blue-500 text-xl">
           <h2 className="font-bold text-gray-800">סיכום</h2>
-          <p className="font-bold text-green-600">סך הכל להצעת המחיר: ₪{totalPrice.toLocaleString()}</p>
+          <p className=" font-semibold text-lime-600">
+            סך הכל להצעת המחיר: ₪{totalPrice.toLocaleString()}
+          </p>
           <p>משך זמן מוערך: {totalDays} ימי עבודה</p>
         </div>
 
@@ -45,10 +61,14 @@ const QuoteItem: React.FC<{
   days: number;
 }> = ({ title, description, price, days }) => (
   <div className="mb-6 p-4 bg-gray-100 rounded-md">
-    <h2 className="text-xl font-bold text-gray-800 mb-2">{title}</h2>
-    <p className="mb-2">{description}</p>
-    <p className="font-bold text-green-600">מחיר: ₪{price.toLocaleString()}</p>
-    <p>משך זמן מוערך: {days} ימי עבודה</p>
+    <h2 className="text-xl font-semibold text-gray-800 mb-2">{title}</h2>
+    <p className="mb-5">{description}</p>
+    <div className="flex justify-between items-center ">
+      <p>{days} ימי עבודה</p>
+      <p className="font-semibold text-lime-600 text-lg">
+        {price.toLocaleString()} ₪
+      </p>
+    </div>
   </div>
 );
 
